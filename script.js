0155e99601e7ea7a4619c56953def96751870e67
ZayyanJavaid <zayyanjawaid4@gmail.com>
2025-02-25 15:33:39 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+ burger = document.querySelector(".burger")
+ navbar = document.querySelector(".resp-parent1")
+ links = document.querySelector(".toggle")
+
+
+
+ burger.addEventListener("click", any1  
+ 
+ )
+
+ function any1(){
+   navbar.classList.toggle("resp-parent1")
+
+   console.log("navbar open & close")
+ }
+
+ 
+
+ 
